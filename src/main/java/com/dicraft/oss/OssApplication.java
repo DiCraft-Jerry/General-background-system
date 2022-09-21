ee872821beadec83e2b29bbe1a3993bb04a3493c
DiCraft <dicraftlover@outlook.com>
2022-09-21 21:36:19 +0800

    启动类

--- src/main/java/com/dicraft/oss/OssApplication.java
+++ src/main/java/com/dicraft/oss/OssApplication.java
@@ -1,8 +1,10 @@
 package com.dicraft.oss;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class OssApplication {
 
