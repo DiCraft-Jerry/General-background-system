0ffb69cfb8e673d92ac1865cf0953261f0b82975
DiCraft <dicraftlover@outlook.com>
2022-09-24 09:26:41 +0800

    文件上传通用常量

---
+++ src/main/java/com/dicraft/oss/ability/Constant.java
@@ -0,0 +1,15 @@
+package com.dicraft.oss.ability;
+
+
+/**
+ * 通用常量
+ * @author DiCraft
+ */
+public class Constant {
+
+
+    /**
+     * 文件上传路径
+     */
+    public static final String FILE_PREFIX = "/upload/file/";
+}
