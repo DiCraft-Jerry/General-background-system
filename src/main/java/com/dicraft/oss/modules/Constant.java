3c2ecf24838cae5966cffcebb46bbcf115e80e58
DiCraft <dicraftlover@outlook.com>
2022-09-25 16:16:10 +0800

    TOKEN通用常量

---
+++ src/main/java/com/dicraft/oss/modules/Constant.java
@@ -0,0 +1,14 @@
+package com.dicraft.oss.modules;
+
+
+/**
+ * 通用常量
+ * @author DiCraft
+ */
+public class Constant {
+
+    /**
+     * 会话
+     */
+    public static final String TOKEN = "token";
+}
