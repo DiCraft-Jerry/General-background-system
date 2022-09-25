ad8019dd9aa49b8ce3ff1addee7ea10e2a282f84
DiCraft <dicraftlover@outlook.com>
2022-09-25 16:25:22 +0800

    系统通用配置Mapper

---
+++ src/main/java/com/dicraft/oss/modules/sys/config/mapper/SysConfigMapper.java
@@ -0,0 +1,15 @@
+package com.dicraft.oss.modules.sys.config.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dicraft.oss.modules.sys.config.entity.SysConfig;
+
+/**
+* <p>
+* 通用配置Mapper
+* </p>
+*
+* @author DiCraft
+*/
+public interface SysConfigMapper extends BaseMapper<SysConfig> {
+
+}
